25a9d1e2a55523cd34400acec5adebf414d35c9b
Rafael Ramos <rafael.ramosr@hotmail.com>
2021-05-06 11:32:13 -0500

    add home page
    - fix: contador js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormContactComponent } from './components/form-contact/form-contact.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormContactComponent
+    FormContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
