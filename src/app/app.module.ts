4d44ff8504095934c337453baf6ff645b499b65c
Rafael Ramos <rafael.ramosr@hotmail.com>
2021-05-07 16:42:58 -0500

    add header and footer into layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 import { FormContactComponent } from './components/form-contact/form-contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
