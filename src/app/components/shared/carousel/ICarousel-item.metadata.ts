169b389caea748e6c34752b824bed2bdad3bae70
@Spare-juice <mdantex@gmail.com>
2021-05-11 15:46:41 -0500

    Corousel add home, bugs

--- src/app/components/shared/carousel/ICarousel-item.metadata.ts
+++ src/app/components/shared/carousel/ICarousel-item.metadata.ts
@@ -1,7 +1,8 @@
 export interface ICarouselItem{
+    marginLeft?: number;
     id: number;
-    title?:{
-        first:string;
+    title?: {
+        first: string;
         second: string;
     };
     subtitle?: string;
--- src/app/components/shared/carousel/carousel.component.ts
+++ src/app/components/shared/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ICarouselItem } from './ICarousel-item.metadata';
 
 @Component({
@@ -13,27 +13,57 @@ export class CarouselComponent implements OnInit {
   @Input() height = 500;
   @Input() isFullScreen = false;
   @Input() items: ICarouselItem[] = [];
-  
+
   /**
    * Final Properties
    */
   public finalHeight: string | number = 0;
   public currentPosition = 0;
 
-  constructor() { 
-    this.finalHeight = this.isFullScreen  ? '100vh' : `${this.height}px`;
-  }
+  constructor() {
+    this.finalHeight = this.isFullScreen ? '100vh' : `${this.height}px`;
+   }
 
+  // tslint:disable-next-line: typedef
   ngOnInit() {
-    this.items.map( (i, index) => {
+    this.items.map( ( i, index ) => {
       i.id = index;
       i.marginLeft = 0;
     });
   }
 
-
-  setCurrentPosition(position: number){
+  // tslint:disable-next-line: typedef
+  setCurrentPosition(position: number) {
     this.currentPosition = position;
-    this.items.find(i => i.id === 0).marginLeft = -100 * position
+    this.items.find(i => i.id === 0).marginLeft = -100 * position;
   }
+
+  // tslint:disable-next-line: typedef
+  setNext() {
+    let finalPercentage = 0;
+    let nextPosition = this.currentPosition + 1;
+    if (nextPosition <= this.items.length - 1) {
+      finalPercentage = -100 * nextPosition;
+    } else {
+      nextPosition = 0;
+    }
+    this.items.find(i => i.id === 0).marginLeft = finalPercentage;
+    this.currentPosition = nextPosition;
+  }
+
+  // tslint:disable-next-line: typedef
+  setBack() {
+    let finalPercentage = 0;
+    let backPosition = this.currentPosition  - 1;
+    if (backPosition >= 0) {
+      finalPercentage = -100 * backPosition;
+    } else {
+      backPosition = this.items.length - 1;
+      finalPercentage = -100 * backPosition;
+    }
+    this.items.find(i => i.id === 0).marginLeft = finalPercentage;
+    this.currentPosition = backPosition;
+
+  }
+
 }
