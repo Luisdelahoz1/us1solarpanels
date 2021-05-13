9f5835b89f20a7aca99cf51c06b086ffea6a157b
@Spare-juice <mdantex@gmail.com>
2021-05-13 17:29:38 -0500

    modify route

--- src/app/interfaces/formspree/formspree.ts
+++ src/app/interfaces/formspree/formspree.ts
@@ -1,8 +1,8 @@
 export interface Formspree {
-  firstName   : String,
-  lastName    : String,
-  address     : String,
-  phoneNumber : Number,
-  email       : String,
-  zipCode     : Number,
+  firstName: String,
+  lastName: String,
+  address: String,
+  phoneNumber: Number,
+  email: String,
+  zipCode: Number,
 }
