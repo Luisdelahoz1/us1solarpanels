ac05b63460025b73a7a79c82b54b5d049969f5ae
Rafael Ramos <rafael.ramosr@hotmail.com>
2021-05-13 10:58:20 -0500

    Receive form data and send it to Form Spree

--- src/app/components/form-contact/form-contact.component.ts
+++ src/app/components/form-contact/form-contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Formspree } from '../../interfaces/formspree/formspree';
+import { FormspreeService } from '../../services/formspree/formspree.service';
 
 @Component({
   selector: 'app-form-contact',
@@ -8,24 +10,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormContactComponent implements OnInit {
 
-  contactForm: FormGroup;
-  submitted: boolean;
+  contactForm : FormGroup;
+  contactData : Formspree;
+  submitted   : boolean;
   // private validEmailPattern = /^[A-Z|.|_|-]+[@]+[A-Z]+.com$/i;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private formSpreeService: FormspreeService) { }
 
   ngOnInit() {
     this.contactForm = this.formBuilder.group({
-      firstName: ['', [Validators.minLength(3), Validators.maxLength(50), Validators.required]],
-      lastName: ['', [Validators.minLength(3), Validators.maxLength(50), Validators.required]],
-      phoneNumber: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.required]],
-      email: ['', [Validators.email, Validators.required]],
-      zipCode: ['', [Validators.minLength(3), Validators.maxLength(4), Validators.required]],
+      firstName   : ['', [Validators.minLength(3), Validators.maxLength(50), Validators.required]],
+      lastName    : ['', [Validators.minLength(3), Validators.maxLength(50), Validators.required]],
+      phoneNumber : ['', [Validators.minLength(10), Validators.maxLength(10), Validators.required]],
+      email       : ['', [Validators.email, Validators.required]],
+      zipCode     : ['', [Validators.minLength(5), Validators.maxLength(9), Validators.required]],
     });
   }
 
   getError(field: string) {
-    if (this.contactForm) {      
+    if (this.contactForm) {
       return this.contactForm.controls[field].errors;
     }
   }
@@ -33,10 +36,19 @@ export class FormContactComponent implements OnInit {
   contactSubmit() {
     this.submitted = true;
     console.log(this.submitted);
-    
 
     if (!this.contactForm.invalid) {
-      console.log(this.contactForm.value);      
+      this.contactData = this.contactForm.value;
+      this.formSpreeService.sendFormSpree(
+        'https://formspree.io/f/xleajbel',
+        this.contactData
+      ).subscribe(response => {
+        console.log(response);
+
+        if (response) {
+          this.contactForm.reset();
+        }
+      });
     }
   }
 
